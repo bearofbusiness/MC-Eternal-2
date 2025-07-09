a32141672749259354f1fe44bff9b68648324460
anabsolutesloth <63694982+anabsolutesloth@users.noreply.github.com>
2025-07-08 23:52:39 -0700

    better safe than sorry with that AN bug it has a storage blacklist too, which prevents the lectern from storing to the container. autopull blacklsit will also inherit from that for safety.

--- overrides/kubejs/server_scripts/Common/tags/ars_nouveau.js
+++ overrides/kubejs/server_scripts/Common/tags/ars_nouveau.js
@@ -23,6 +23,13 @@ ServerEvents.tags("block", event => {
     //Storage Autopull Blacklist
     // prevents the Storage Lectern from trying to put away things in adjacent containers whose source blocks are on this list
     event.add("ars_nouveau:storage/autopull_disabled", [
+        "#ars_nouveau:lectern_blacklist"
+    ])
+
+
+    //Storage Interaction Blacklist
+    // blocks in this tag can't be used by the Storage Lectern as containers
+    event.add("ars_nouveau:lectern_blacklist", [
         //can dupe shit in drives if it doesn't find somewhere to put them. atleast i think that's what the problem is.
         "ae2:drive",
         "ae2:chest",
