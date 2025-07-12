b07a69ce45932297d5aba6b636b45c50660244d2
anabsolutesloth <63694982+anabsolutesloth@users.noreply.github.com>
2025-07-12 00:36:46 -0700

    a bit of botania quest fancying up and tweaks - rune quest gives 200 coins now since it requires collecting all 16 runes - terraplate quest gives 100 coins - runic altar quest now requires the advancement for crafting on it - petal apothecary no longer requires looking at an apothecary with water, cause this could only detect a regular one. it also accepts any apothecary now. - pure daisy quest no longer requires bark livingwood. - added some decorations to the questline and changed some quest shapes, i'm too tired to bother making them too pretty atm, might also need a bit of advice. - updated custom tags for botania to be under "mce2" instead of "mce"

--- overrides/kubejs/server_scripts/Common/tags/botania.js
+++ overrides/kubejs/server_scripts/Common/tags/botania.js
@@ -1,13 +1,15 @@
 ServerEvents.tags("item", event => {
 
-    event.add('mce:botania/basic_generating_flower', [
+    event.add('mce2:botania/basic_generating_flower', [
         'botania:endoflame',
         'botania:hydroangeas'
     ])
     
-    event.add('mce:botania/corporea_automation_device', [
+    event.add('mce2:botania/corporea_automation_device', [
         'botania:corporea_funnel',
         'botania:corporea_interceptor',
         'botania:corporea_retainer'
     ])
+
+    event.add("mce2:botania/petal_apothecary", /botania:apothecary_.*/)
 })
