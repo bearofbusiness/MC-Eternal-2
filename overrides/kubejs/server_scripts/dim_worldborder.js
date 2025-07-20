dc9bb18593f7803873f57ccd7e62a6d4f2af6898
adam <57104144+adam9899@users.noreply.github.com>
2025-07-20 15:58:19 +0100

    worldborder script for cata dimensions

---
+++ overrides/kubejs/server_scripts/dim_worldborder.js
@@ -0,0 +1,34 @@
+
+ServerEvents.loaded(event => {
+  event.server.runCommandSilent('execute in cataclysm_dimension:cataclysm_abyssal_depths run worldborder center 0 0')
+
+  event.server.runCommandSilent('execute in cataclysm_dimension:cataclysm_abyssal_depths run worldborder set 500')
+
+  event.server.runCommandSilent('execute in cataclysm_dimension:cataclysm_bastion_lost run worldborder center 0 0')
+
+  event.server.runCommandSilent('execute in cataclysm_dimension:cataclysm_bastion_lost run worldborder set 500')
+
+  event.server.runCommandSilent('execute in cataclysm_dimension:cataclysm_eternal_frosthold run worldborder center 0 0')
+
+  event.server.runCommandSilent('execute in cataclysm_dimension:cataclysm_eternal_frosthold run worldborder set 500')
+
+  event.server.runCommandSilent('execute in cataclysm_dimension:cataclysm_forge_of_aeons run worldborder center 0 0')
+
+  event.server.runCommandSilent('execute in cataclysm_dimension:cataclysm_forge_of_aeons run worldborder set 500')
+
+  event.server.runCommandSilent('execute in cataclysm_dimension:cataclysm_infernos_maw run worldborder center 0 0')
+
+  event.server.runCommandSilent('execute in cataclysm_dimension:cataclysm_infernos_maw run worldborder set 500')
+
+  event.server.runCommandSilent('execute in cataclysm_dimension:cataclysm_pharaohs_bane run worldborder center 0 0')
+
+  event.server.runCommandSilent('execute in cataclysm_dimension:cataclysm_pharaohs_bane run worldborder set 500')
+
+  event.server.runCommandSilent('execute in cataclysm_dimension:cataclysm_sanctum_fallen run worldborder center 0 0')
+
+  event.server.runCommandSilent('execute in cataclysm_dimension:cataclysm_sanctum_fallen run worldborder set 500')
+
+  event.server.runCommandSilent('execute in cataclysm_dimension:cataclysm_souls_anvil run worldborder center 0 0')
+  
+  event.server.runCommandSilent('execute in cataclysm_dimension:cataclysm_souls_anvil run worldborder set 500')
+})
