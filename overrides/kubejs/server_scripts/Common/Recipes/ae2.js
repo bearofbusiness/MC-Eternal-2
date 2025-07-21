a51e24a5a9baca41e17169dde5197c6efe6882e6
anabsolutesloth <63694982+anabsolutesloth@users.noreply.github.com>
2025-07-21 15:58:49 -0700

    Removed Elite Control Circuit from crossdim wireless storage accesor recipes - yeah that's fair it was a bit far - AE2 card has it stepped back to an advanced circuit. - the rest have it completely removed.

--- overrides/kubejs/server_scripts/Common/Recipes/ae2.js
+++ overrides/kubejs/server_scripts/Common/Recipes/ae2.js
@@ -6,7 +6,7 @@ ServerEvents.recipes(event => {
     ], {
         A: "ae2:advanced_card",
         W: "waystones:warp_stone",
-        C: "mekanism:elite_control_circuit",
+        C: "mekanism:advanced_control_circuit",
         E: "minecraft:echo_shard"
     }).id("ae2wtlib:quantum_bridge_card")
 
